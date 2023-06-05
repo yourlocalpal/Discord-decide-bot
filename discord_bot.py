3cb6691689a66e0ae5ff30cf1483db5d2112ae0b
yourlocalpal <118146578+yourlocalpal@users.noreply.github.com>
2023-06-05 02:55:08 -0500

    .choice added functionality
    improved .choice process

--- discord_bot.py
+++ discord_bot.py
@@ -4,36 +4,54 @@ from dotenv import load_dotenv
 import random
 
 load_dotenv()
-
 client = discord.Client(intents=discord.Intents.all())
 
-def decide(options):
+
+def choice(options):
     return random.choice(options)
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    
-    if '.decide' in message.content:
+
+    if message.content.startswith('.decide'):
         with open('log.txt', 'a') as f:
             f.write(f'{message.author.name} requested: {message.content}\n')
         question = message.content.lower().replace('.decide', '').strip()
-        rand = random.random() # generate a random float between 0 and 1
+        rand = random.random()  # generate a random float between 0 and 1
         if rand < 0.5:
             response = 'No'
         else:
             response = 'Yes'
         await message.channel.send(response)
-        
-    if message.content.startswith('.choice'):
-        options = message.content.split(' ')[1:]
-        decision = decide(options)
-        await message.channel.send(decision)
+
+    elif message.content.startswith('.choice'):
+        sentence = message.content.lower().replace('.choice', '').strip()
+        # Extract choices by removing "or" and splitting on ","
+        choices = sentence.replace(' or ', ',').split(',')
+
+        # Remove any leading/trailing whitespace from choices
+        choices = [choice.strip() for choice in choices]
+
+        # Detecting valid choice question
+        if len(choices) > 1:
+            decision = choice(choices)
+            with open('log.txt', 'a') as f:
+                f.write(f'{message.author.name} requested: {message.content}\n')
+            await message.channel.send(decision)
+        else:
+            with open('log.txt', 'a') as f:
+                f.write(f'{message.author.name} requested an invalid choice: {message.content}\n')
+            await message.channel.send("Please provide multiple choices.")
+
+
 
 if __name__ == "__main__":
-    client.run(os.getenv("DISCORD_TOKEN"))
+    client.run('DISCORD API')
